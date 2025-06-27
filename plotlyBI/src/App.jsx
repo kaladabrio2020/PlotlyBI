c3fb7c2620b2cbe2f210aa57e650984361d3cd7e
Mateus Sousa do Carmo <mateus.1090ptbr0@gmail.com>
2025-06-27 20:00:32 -0300

    0.0.1 - MSC01 - Iniciando Trabalho * Iniciando o MSC01 * Criando pastas * Desenvolvendo

--- plotlyBI/src/App.jsx
+++ plotlyBI/src/App.jsx
@@ -1,38 +1,125 @@
 import React from "react";
 
+import { Button, Dropdown, Space, Select } from 'antd';
+import { FileOutlined } from '@ant-design/icons';
+
+// icons
+import postgresIcon   from "./assets/icons/postgres.png";
+import duckdbIcon     from "./assets/icons/duckdb.png";
+import mysqlIcon      from "./assets/icons/mysql.png";
+import bigqueryIcon   from "./assets/icons/google.png";
+
+
 // Styles 
 import "./styles/App.css";
 
+
 // Components
+
+const colourOptions = [
+    { value: 'ocean', label: 'Ocean', color: '#00B8D9' },
+    { value: 'blue', label: 'Blue', color: '#0052CC' },
+    { value: 'purple', label: 'Purple', color: '#5243AA' },
+    { value: 'red', label: 'Red', color: '#FF5630' },
+];
+const menuItems = [
+  { key: "1", label: "Import csv file", icon: <FileOutlined /> },
+  { 
+    key: "2", 
+    label:(
+        <Space>
+            <img src={postgresIcon} alt="Postgres" style={{ width: 13, height: 13 }} />
+            Postgres 
+        </Space>
+    )
+  },
+  { 
+    key: "3", 
+    label: (
+        <Space>
+            <img src={mysqlIcon} alt="MySQL" style={{ width: 13, height: 13 }} />
+            MySQL
+        </Space>
+    )
+ },
+  { 
+    key: "4", 
+    label: (
+        <Space>
+            <img src={duckdbIcon} alt="DuckDB" style={{ width: 13, height: 13 }} />
+            DuckDB
+        </Space>
+    ) 
+
+  },
+  { 
+    key: "5", 
+    label: (
+        <Space>
+            <img src={bigqueryIcon} alt="BigQuery" style={{ width: 12, height: 12 }} />
+            BigQuery
+        </Space>
+    ) 
+  },
+];
 
 export default function App() {
     return (
         <>
         <div>
             <div className="bar-top-home">
-                <div className="div-connection-seelect">
-                    <div>
-                        <button>Connect</button>
+
+                <div className="div-connection-select">
+                    <div className="div-connection">
+                        <Space wrap>
+                            <Dropdown menu={{ items: menuItems }} placement="bottom" arrow trigger={['click']}> 
+                                <Button type="primary">
+                                    Connection
+                                </Button>
+                            </Dropdown>
+                        </Space>
                     </div>
                     <div>
-                        <select>
-                            <option>Seelect</option>
-                        </select>
+                        <Space>
+                            <Select
+                                style={{ width: 250 }}
+                                options={colourOptions}
+                                placeholder="Select a table"
+                                allowClear
+                            />
+                        </Space>
                     </div>
                 </div>
+
                 <div className="div-metrics-dimensions">
-                    <div>
+                    <div className="div-dimensions">
                         <p>Dimensions</p>
+        
+                        <p>Metrics</p>
                     </div>
-                    <div>
-                        <div>
-                            <p>Metrics</p>
-                        </div>
+                    <div className="div-multiple-select-metrics-dimensions">
                         <div>
                             
+                        <Select
+                            mode="multiple"
+                            options={colourOptions}
+                            style={{ width: 700 , marginTop: 10, marginBottom: 5, height:30}}
+                            allowClear
+                        />
                         </div>
+                        <div>
+
+                        <Select
+                            mode="multiple"
+                            options={colourOptions}
+                            style={{ width: 700 ,height:30}}
+                            allowClear
+                        />
+                        </div>
+
                     </div>
                 </div>
+
             </div>
         </div>
         </>
